32c63226d46fc33b7a720408d9e13365aac60a0b
fahime <f.delkhoshian@gmail.com>
2019-05-22 11:58:29 +0430

    insert new comment And get comments

--- app/controllers/api/v1/commentController.js
+++ app/controllers/api/v1/commentController.js
@@ -1,6 +1,59 @@
 const Controller = require('app/controllers/controller');
 
-module.exports = new class PostController extends Controller{
+module.exports = new class CommentController extends Controller{
 
+     /**
+     * 
+     * url: api/v1/comment
+     * method : POST
+     * params : [post_id, content]
+     */
+    async insert(req, res) {
+        try {
+
+            let newComment = new this.model.Comment({
+                post : req.body.post_id,
+                content : req.body.content,
+            });
+
+            await newComment.save((err, comment) => {
+                if(err) {
+                    return this.failedMessage(err, res);
+                } else{
+                    return res.json({
+                        message : 'comment was successfully registered',
+                        success : true
+                    }); 
+                }                
+            });
+        
+        } catch(err) {
+            this.failedMessage(err.message , res);
+        }  
+    }
+
+     /**
+     * 
+     * url: api/v1/comment
+     * method : GET
+     * params : [post_id]
+     */
+    async getComments(req, res) {
+        try {
+            const comments =await this.model.Comment.find({post : req.body.post_id});
+            return res.json({
+                data : comments.map(comment =>{
+                    return {
+                        id : comment._id,
+                        content : comment.content,
+                    }
+                }),
+                success : true
+            });
+
+        } catch (err) {
+            this.failedMessage(err.message , res);
+        }
+    }
 
 }
--- app/routes/api/v1/index.js
+++ app/routes/api/v1/index.js
@@ -5,6 +5,7 @@ const router = express.Router();
 //Controllers
 const userController = require('app/controllers/api/v1/userController');
 const postController = require('app/controllers/api/v1/postController');
+const commentController = require('app/controllers/api/v1/commentController');
 
 //Validators
 const userValidator = require('app/validators/userValidator');
@@ -17,5 +18,8 @@ router.post('/user', userValidator.signIn(), userController.insert);
 
 router.post('/post', ApiAuth , uploadImage.single('image'),postController.insert);
 router.get('/post',postController.getPosts);
+
+router.post('/comment',commentController.insert);
+router.get('/comment',commentController.getComments);
 
 module.exports = router;
